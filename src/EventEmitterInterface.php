80f44a7d10c337b2023ff3ef7ed33a27b0953686
Charlotte Dunois <github@charuru.moe>
2018-09-03 08:27:15 +0200

    Add scalar type declarations

--- src/EventEmitterInterface.php
+++ src/EventEmitterInterface.php
@@ -15,47 +15,43 @@ interface EventEmitterInterface {
      * @param string    $event
      * @param callable  $listener
      * @return $this
-     * @throws \InvalidArgumentException
      */
-    function on($event, callable $listener);
+    function on(string $event, callable $listener);
     
     /**
      * Attach a listener to an event, for exactly once.
      * @param string    $event
      * @param callable  $listener
      * @return $this
-     * @throws \InvalidArgumentException
      */
-    function once($event, callable $listener);
+    function once(string $event, callable $listener);
     
     /**
      * Remove specified listener from an event.
      * @param string    $event
      * @param callable  $listener
      * @return $this
-     * @throws \InvalidArgumentException
      */
-    function removeListener($event, callable $listener);
+    function removeListener(string $event, callable $listener);
     
     /**
      * Remove all listeners from an event (or all listeners).
      * @param string|null  $event
      * @return $this
      */
-    function removeAllListeners($event = null);
+    function removeAllListeners(?string $event = null);
     
     /**
      * Get listeners for a specific events, or all listeners.
      * @param string|null  $event
      * @return array
      */
-    function listeners($event = null);
+    function listeners(?string $event = null);
     
     /**
      * Emits an event, catching all exceptions and emitting an error event for these exceptions.
      * @param string  $event
      * @param mixed   $arguments
-     * @throws \InvalidArgumentException
      */
-    function emit($event, ...$arguments);
+    function emit(string $event, ...$arguments);
 }
